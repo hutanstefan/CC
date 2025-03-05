d10544b61e70efa911ed1a6a9d67b7b58fff7ca9
Hutan Stefan <61848535+hutanstefan@users.noreply.github.com>
2025-03-05 18:07:02 +0200

    lab2 init

---
+++ resources/views/lab2.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab2</title>
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+<div>
+    Test
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/lab1', function () {
     return view('lab1');
 });
 Route::get('/get-random-number', [RandomNumberController::class, 'getRandomNumber']);
+
+Route::get('/lab2', function () {
+    return view('lab2');
+});
